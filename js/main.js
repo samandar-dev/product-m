2cae5769f9f8799ec99cbba63201be50b137a3aa
samandar-dev <samandarbohodirov01@gmail.com>
2022-02-06 21:21:26 +0500

    creat

--- js/main.js
+++ js/main.js
@@ -6,25 +6,25 @@ let elMenuBtn = document.getElementById('menuBtn');
 let elMenuBox = document.getElementById('menuBox');
 let elMenuRemov = document.getElementById('menuRemov')
 
-// elMenuBtn.addEventListener('click', function () {
-//   elMenuBox.classList.add('open-menu');
-// })
-
-// elMenuRemov.addEventListener('click', function () {
-//   elMenuBox.classList.add('menu-remov');
-// })
-
-let count = 0;
 elMenuBtn.addEventListener('click', function () {
-  elMenuBox.classList.toggle('open-menu');
-  if (count == 0) {
-    elMenuBtn.classList = 'open-menu';
-    count++;
-  }
-  else {
-    elMenuBox.classList.toggle('menu-remov');
-    count = 0;
-  }
-});
+  elMenuBox.classList.add('open-menu');
+})
+
+elMenuRemov.addEventListener('click', function () {
+  elMenuBox.classList.add('menu-remov');
+})
+
+// let count = 0;
+// elMenuBtn.addEventListener('click', function () {
+//   elMenuBox.classList.toggle('open-menu');
+//   if (count == 0) {
+//     elMenuBtn.classList = 'open-menu';
+//     count++;
+//   }
+//   else {
+//     elMenuBox.classList.toggle('menu-remov');
+//     count = 0;
+//   }
+// });
 
 
